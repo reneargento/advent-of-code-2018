1bcc2fc150c48d553cc76fd185ce9b252ff94cba
Rene Argento <rene.argento@gmail.com>
2018-12-06 16:56:12 +0100

    Updated day 6-2 solution

--- src/Day6_2.java
+++ src/Day6_2.java
@@ -35,54 +35,10 @@ public class Day6_2 {
             coordinates.add(new Coordinate(row, column));
         }
 
-        for (Coordinate source : coordinates) {
-            floodFill(matrix, source, areas);
-        }
-
         int regionSize = computeRegionSize(coordinates, areas);
         System.out.println("Region size: " + regionSize);
     }
 
-    private static void floodFill(int[][] matrix, Coordinate source, int[][] areas) {
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.offer(source);
-        int sourceId = getCellId(source.row, source.column, matrix[0].length);
-
-        int[] neighborRows = {0, -1, 1, 0};
-        int[] neighborColumns = {-1, 0, 0, 1};
-        boolean[] visited = new boolean[areas.length * areas[0].length];
-
-        while (!queue.isEmpty()) {
-            Coordinate currentCoordinate = queue.poll();
-            int row = currentCoordinate.row;
-            int column = currentCoordinate.column;
-
-            for (int k = 0; k < 4; k++) {
-                int neighborRow = row + neighborRows[k];
-                int neighborColumn = column + neighborColumns[k];
-
-                if (isValidCell(neighborRow, neighborColumn, matrix)) {
-                    int neighborCellId = getCellId(neighborRow, neighborColumn, matrix[0].length);
-
-                    if (!visited[neighborCellId]) {
-                        int manhattanDistance = getManhattanDistance(source, new Coordinate(neighborRow, neighborColumn));
-
-                        if (manhattanDistance <= matrix[neighborRow][neighborColumn]) {
-                            areas[neighborRow][neighborColumn] = -1;
-                            queue.offer(new Coordinate(neighborRow, neighborColumn));
-                        }
-
-                        if (manhattanDistance < matrix[neighborRow][neighborColumn]) {
-                            areas[neighborRow][neighborColumn] = sourceId;
-                            matrix[neighborRow][neighborColumn] = manhattanDistance;
-                        }
-
-                        visited[neighborCellId] = true;
-                    }
-                }
-            }
-        }
-    }
 
     private static int computeRegionSize(List<Coordinate> coordinates, int[][] areas) {
         int targetDistance = 10000;
@@ -103,14 +59,6 @@ public class Day6_2 {
         }
 
         return regionSize;
-    }
-
-    private static boolean isValidCell(int row, int column, int[][] matrix) {
-        return row >= 0 && row < matrix.length && column >= 0 && column < matrix[0].length;
-    }
-
-    private static int getCellId(int row, int column, int maxColumn) {
-        return (row * maxColumn) + column;
     }
 
     private static int getManhattanDistance(Coordinate coordinate1, Coordinate coordinate2) {
